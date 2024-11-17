a4a74a2f5017043b5dd6b15dec3f7b36a6bce061
Thegpsgameboy <Thegpsgameboy@gmail.com>
2024-11-17 00:18:16 -0500

    new

---
+++ SinWave.py
@@ -0,0 +1,17 @@
+#Online Source
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Generate x values from 0 to 2*pi
+x = np.linspace(0, 2 * np.pi, 100)
+
+# Calculate y values using the sine function
+y = np.sin(x)
+
+# Create the plot
+plt.plot(x, y)
+plt.xlabel('x')
+plt.ylabel('sin(x)')
+plt.title('Sine Graph')
+plt.grid(True)
+plt.show()
